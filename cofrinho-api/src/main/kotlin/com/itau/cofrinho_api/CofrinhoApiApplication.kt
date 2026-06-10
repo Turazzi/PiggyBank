4b2c488977ee40fe25a901ed71652e6b73f39a64
Turazzi <analivia.turazzi@gmail.com>
2026-06-09 22:00:07 -0300

    chore: initialize spring boot project with core dependencies

---
+++ cofrinho-api/src/main/kotlin/com/itau/cofrinho_api/CofrinhoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.itau.cofrinho_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CofrinhoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CofrinhoApiApplication>(*args)
+}
